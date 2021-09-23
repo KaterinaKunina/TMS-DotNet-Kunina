37f35b21c78ee0813a111c88c07c536ba2c474a5
Katerina Kunina <Katerina.kunina08@gmail.com>
2021-09-23 22:15:11 +0300

    feat: Add solution of homework1

---
+++ src/WhatDayIsToday/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WhatDayIsToday
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter Date:");
+            string str = Console.ReadLine();
+
+            try
+            {
+                var date = DateTime.Parse(str);
+                Console.WriteLine($"Day is   \"{date.DayOfWeek}\"\n");
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid format.");
+                return;
+            }
+
+
+        }
+    }
+}
