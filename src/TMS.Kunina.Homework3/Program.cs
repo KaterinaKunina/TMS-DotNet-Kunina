2f010959879d1ba7aeaf489a6e030f4e9b870df5
Katerina Kunina <Katerina.kunina08@gmail.com>
2021-09-27 22:10:46 +0300

    add homework3

---
+++ src/TMS.Kunina.Homework3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TMS.Kunina.Homework3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Date:");
+            string str = Console.ReadLine();
+
+            MainClass mc = new MainClass();
+            mc.whatDayOfWeek(str);
+
+        }
+
+        public void whatDayOfWeek (string str)
+        {
+            try
+            {
+                var date = DateTime.Parse(str);
+                Console.WriteLine($"Day is   \"{date.DayOfWeek}\"\n");
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid format.");
+                return;
+            }
+        }
+    }
+}
