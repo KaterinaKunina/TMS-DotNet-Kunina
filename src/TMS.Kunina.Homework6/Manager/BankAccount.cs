acedec45558e083dbbd7f665d181ad505864547b
Katerina Kunina <Katerina.kunina08@gmail.com>
2021-10-18 00:13:23 +0300

    Add homework 6

---
+++ src/TMS.Kunina.Homework6/Manager/BankAccount.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Manager
+{
+    public class BankAccount
+    {
+        public event EventHandler<bool> ProcessCompleted;
+
+        private double money;
+        private string name;
+
+        public BankAccount(double money, string name)
+        {
+            this.money = money;
+            this.name = name;
+        }
+
+        public void AddMoney(double money)
+        {
+            Console.WriteLine(name + " add " + money);
+            this.money += money;
+            OnProcessCompleted(true);
+
+        }
+
+        public void Subtract(double money)
+        {
+            if (this.money - money < 0)
+            {
+                Console.WriteLine("Insufficient funds on the account!");
+                Console.WriteLine(this.name + "wants to spent " + money);
+                getInfo();
+                OnProcessCompleted(false);
+            }
+            else
+            {
+                this.money -= money;
+                Console.WriteLine(name + " spent money =  " + money);
+                OnProcessCompleted(true);
+            }
+        }
+
+        public void getInfo()
+        {
+            Console.WriteLine("Balance of " + this.name + ": " + this.money);
+        }
+
+        protected virtual void OnProcessCompleted(bool IsSuccessful)
+        {
+            ProcessCompleted?.Invoke(this.name, IsSuccessful);
+        }
+    }
+}
---
+++ src/TMS.Kunina.Homework6/Ui/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Manager;
+
+namespace Ui
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount person1 = new BankAccount(1000, "Kris Boil");
+            BankAccount person2 = new BankAccount(5000, "Dominique Francon");
+
+            person1.ProcessCompleted += ProcessCompleted;
+            person2.ProcessCompleted += ProcessCompleted;
+
+            person1.getInfo();
+            person1.AddMoney(10);
+            person1.Subtract(5000);
+            person1.getInfo();
+
+            person2.getInfo();
+            person2.AddMoney(10);
+            person2.Subtract(5000);
+            person2.Subtract(500);
+            person2.getInfo();
+
+            person1.Subtract(500);
+            person1.getInfo();
+
+            Console.WriteLine("\n****************************");
+            Console.WriteLine("Added person without event");
+            Console.WriteLine("****************************");
+            BankAccount person3 = new BankAccount(0, "Elizabeth Olsen");
+            person3.Subtract(10);
+
+        }
+
+        public static void ProcessCompleted(object sender, bool IsSuccessful)
+        {
+            Console.WriteLine("Process " + (IsSuccessful ? $"Completed Successfully for {sender}" : "failed"));
+            Console.WriteLine("___________________________________________");
+        }
+
+    }
+}
