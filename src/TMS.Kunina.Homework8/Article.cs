7302f6a5f181dfa24403d3bb2c57916ef5109aa8
Katerina Kunina <Katerina.kunina08@gmail.com>
2021-10-25 16:45:13 +0300

    Add homework8

---
+++ src/TMS.Kunina.Homework8/Article.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace TMS.Kunina.Homework8
+{
+    public class Article
+    {
+        private int id;
+        private string productName;
+        private string storeName;
+        private double price;  // Price in BYN
+
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public string ProductName
+        {
+            get { return productName; }
+            set { productName = value; }
+        }
+
+        public string StoreName
+        {
+            get { return storeName; }
+            set { storeName = value; }
+        }
+
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+    }
+}
+
+
